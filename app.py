37fa3fcc67e54bc651b417cd2c9f9de741a2baed
barouu78-wq <barouu78@gmail.com>
2026-09-24 15:06:45 +0200

    Brancher les pages SEO sur le module dédié

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from dotenv import load_dotenv
 from flask import Flask, Response, jsonify, request, stream_with_context
 from openai import OpenAI
 from werkzeug.middleware.proxy_fix import ProxyFix
+from services.seo import render_seo_page
 
 load_dotenv()
 
