5cf4f5564f128197b03503e02b6f357c73ae84b9
barouu78-wq <barouu78@gmail.com>
2026-09-24 16:47:49 +0200

    Extraire la recherche cartographique de app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ from dotenv import load_dotenv
 from flask import Flask, Response, jsonify, request, stream_with_context
 from openai import OpenAI
 from werkzeug.middleware.proxy_fix import ProxyFix
-from services.seo import render_seo_page
 from services.seo import SEO_PAGES, render_seo_page
 from services.explorer import render_explorer_page
+from services.maps import lookup_map
 from services.images import (
     fetch_city_image as _fetch_city_image,
     fetch_commons_image as _fetch_commons_image,
@@ -669,61 +669,6 @@ def fetch_topic_images(message):
                 return photos
 
     return photos or None
-
-
-MAP_PLACES = (
-    ("aibd", "Aéroport Blaise Diagne Diass Sénégal", "Aéroport AIBD"),
-    ("aéroport", "Aéroport Blaise Diagne Diass Sénégal", "Aéroport AIBD"),
-    ("maison des esclaves", "Maison des Esclaves Gorée Sénégal", "Maison des Esclaves"),
-    ("île de gorée", "Île de Gorée Sénégal", "Île de Gorée"),
-    ("ile de goree", "Île de Gorée Sénégal", "Île de Gorée"),
-    ("gorée", "Île de Gorée Sénégal", "Île de Gorée"),
-    ("goree", "Île de Gorée Sénégal", "Île de Gorée"),
-    ("lac rose", "Lac Retba Sénégal", "Lac Rose"),
-    ("lac retba", "Lac Retba Sénégal", "Lac Rose"),
-    ("cap skirring", "Cap Skirring Sénégal", "Cap Skirring"),
-    ("saint-louis", "Saint-Louis Sénégal", "Saint-Louis"),
-    ("saint louis", "Saint-Louis Sénégal", "Saint-Louis"),
-    ("monument de la renaissance", "Monument de la Renaissance africaine Dakar", "Monument de la Renaissance"),
-    ("joal", "Joal-Fadiouth Sénégal", "Joal-Fadiouth"),
-    ("fadiouth", "Joal-Fadiouth Sénégal", "Joal-Fadiouth"),
-    ("touba", "Grande Mosquée de Touba Sénégal", "Touba"),
-    ("ziguinchor", "Ziguinchor Sénégal", "Ziguinchor"),
-    ("saly", "Saly Portudal Sénégal", "Saly"),
-    ("thiès", "Thiès Sénégal", "Thiès"),
-    ("thies", "Thiès Sénégal", "Thiès"),
-    ("kaolack", "Kaolack Sénégal", "Kaolack"),
-    ("mbour", "M'Bour Sénégal", "M'Bour"),
-    ("somone", "Somone Sénégal", "Somone"),
-    ("popenguine", "Popenguine Sénégal", "Popenguine"),
-    ("toubab dialaw", "Toubab Dialaw Sénégal", "Toubab Dialaw"),
-    ("fatick", "Fatick Sénégal", "Fatick"),
-    ("louga", "Louga Sénégal", "Louga"),
-    ("matam", "Matam Sénégal", "Matam"),
-    ("tambacounda", "Tambacounda Sénégal", "Tambacounda"),
-    ("kédougou", "Kédougou Sénégal", "Kédougou"),
-    ("kedougou", "Kédougou Sénégal", "Kédougou"),
-    ("kolda", "Kolda Sénégal", "Kolda"),
-    ("sédhiou", "Sédhiou Sénégal", "Sédhiou"),
-    ("sedhiou", "Sédhiou Sénégal", "Sédhiou"),
-    ("diamniadio", "Diamniadio Sénégal", "Diamniadio"),
-    ("pointe sarène", "Pointe Sarène Sénégal", "Pointe Sarène"),
-    ("pointe sarene", "Pointe Sarène Sénégal", "Pointe Sarène"),
-    ("dakar", "Dakar Sénégal", "Dakar"),
-)
-
-
-def lookup_map(message):
-    lowered = message.lower()
-    for key, query, label in MAP_PLACES:
-        if key in lowered:
-            q = quote(query)
-            return {
-                "label": label,
-                "url": "https://www.google.com/maps/search/?api=1&query=" + q,
-                "embed": "https://maps.google.com/maps?q=" + q + "&hl=fr&z=14&output=embed",
-            }
-    return None
 
 
 def should_use_web(message):
